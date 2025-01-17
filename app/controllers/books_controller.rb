994a3c50d660e687545f1a3a4027b6a6e4884294
EC2 Default User <ec2-user@ip-172-31-13-31.ap-northeast-1.compute.internal>
2025-01-17 08:22:56 +0000

    [Add]全部の画面の確認から

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,6 +10,7 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
+    @user = @book.user
   end
 
   def show
@@ -25,8 +26,14 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
   private
 
   def book_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,13 +24,13 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to book_user_path(@user.id)
+    redirect_to user_path(@user.id)
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
 end
